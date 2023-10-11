ed3dee6802d8fa566bd22e867ddec6b4fa5783b6
Dom Loong <dom.loong@freshbooks.com>
2023-10-10 22:41:20 -0400

    Update script

--- compress_jpeg.py
+++ compress_jpeg.py
@@ -1,19 +1,27 @@
 from PIL import Image
 from pathlib import Path
 
+import shutil
 import os
 
 gallery_path = Path(".") / "gallery"
 
-for i, im_path in enumerate(gallery_path.glob("**/*")):
+for im_path in gallery_path.glob("**/*"):
     im = Image.open(im_path)
+    dest_path = Path(".") / f"output/{im_path.name}"
+    quality = 100
 
-    dest_path = Path(".") / f"output/img_{i+1}.jpg"
+    print(f"Now compressing {im_path.name}")
 
     if os.path.getsize(im_path) > 16000000:
-        im.save(dest_path, optimize=True)
-    else:
-        im.save(dest_path)
+        im.save(dest_path, optimize=True, quality=quality)
+        while os.path.getsize(dest_path) > 16000000:
+            quality -= 1
+            im.save(dest_path, optimize=True, quality=quality)
 
-    if os.path.getsize(dest_path) > 16000000:
-        print(dest_path + " still over 16 MB!")
+        print(f"Compressed with quality {quality}")
+    else:
+        print(f"{im_path.name} does not need compressing")
+        shutil.copyfile(im_path, dest_path)
+
+    print(f"Saved {im_path.name} at size: {os.path.getsize(dest_path)}")
